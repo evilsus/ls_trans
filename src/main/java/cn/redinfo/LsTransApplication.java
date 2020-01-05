ce5ace3972dcf0f16dd25ded7067b65aed4426c4
liyl <450638090@qq.com>
2020-01-05 21:05:51 +0800

    5

--- src/main/java/cn/redinfo/LsTransApplication.java
+++ src/main/java/cn/redinfo/LsTransApplication.java
@@ -6,10 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LsTransApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(LsTransApplication.class, args);
 
-		//111
+		//112
 	}
 
 }
